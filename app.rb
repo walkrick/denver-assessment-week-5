85107f17821301c3f3e7bde706dcbef8119a71a3
Hunter Gillane <hunter.gillane@gmail.com>
2014-07-06 20:19:01 -0600

    add some users on initialize

--- app.rb
+++ app.rb
@@ -8,10 +8,13 @@ class ContactsApp < Sinatra::Base
     super
     @contact_database = ContactDatabase.new
     @user_database = UserDatabase.new
+
+    @user_database.insert(username: "Hunter", password: "puglyfe")
+    @user_database.insert(username: "Spencer", password: "spen123")
   end
 
   get "/" do
-    "Hello"
+    "Hello week 5"
   end
 
 end
